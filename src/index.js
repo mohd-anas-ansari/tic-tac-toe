26c83e8a783cb4b8ee5dc55bdd14a4c982dfbc47
anasansari157 <anasansari157@gmail.com>
2020-06-08 16:03:15 +0530

    Add styles

--- src/index.js
+++ src/index.js
@@ -92,32 +92,35 @@ class Board extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<div className="board-row">
-					{this.renderSquare(0)}
-					{this.renderSquare(1)}
-					{this.renderSquare(2)}
+			<>
+				<h1>Tic Tac Toe</h1>
+				<div className="board-container">
+					<div className="board-row">
+						{this.renderSquare(0)}
+						{this.renderSquare(1)}
+						{this.renderSquare(2)}
+					</div>
+					<div className="board-row">
+						{this.renderSquare(3)}
+						{this.renderSquare(4)}
+						{this.renderSquare(5)}
+					</div>
+					<div className="board-row">
+						{this.renderSquare(6)}
+						{this.renderSquare(7)}
+						{this.renderSquare(8)}
+					</div>
 				</div>
-				<div className="board-row">
-					{this.renderSquare(3)}
-					{this.renderSquare(4)}
-					{this.renderSquare(5)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(6)}
-					{this.renderSquare(7)}
-					{this.renderSquare(8)}
-				</div>
-			</div>
+			</>
 		);
 	}
 }
 
 function Square(props) {
 	return (
-		<button className="square" onClick={props.onClick}>
+		<div className="square" onClick={props.onClick}>
 			{props.value}
-		</button>
+		</div>
 	);
 }
 
